a4147bbb73ba20099df85b044a1145e6c4e838cd
sohamdp <sohamdp28@gmail.com>
2023-03-18 21:45:53 +0530

    Added Logs

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,87 +1,14 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Calculator
 {
-    public double sqrt(double x)
-    {
-        double result=0;
-        try
-        {
-            if(x<0)
-            {
-                result=Double.NaN;
-                throw new ArithmeticException("square root of negative number is not possible");
-            }
-            else
-            {
-                result=Math.sqrt(x);
-            }
-        }
-        catch (ArithmeticException error)
-        {
-            System.out.println(error.getLocalizedMessage());
-        }
-        return result;
 
-    }
-    public double factorial(double x)
-    {
-        return fact(x);
-    }
-    public double fact(double x)
-    {
-        Integer temp=1;
-        for(Integer i=1;i<=x;i++)
-        {
-            temp=i*temp;
-        }
-        return temp;
-    }
-    public  double logarithm(double x)
-    {
-        double result=0;
-        try
-        {
-            if(x<=0)
-            {
-                result=Double.NaN;
-                throw new ArithmeticException("Natural Log for 0 and negative number is not possible");
-            }
-            else
-            {
-                result=Math.log(x);
-            }
-        }
-        catch (ArithmeticException error)
-        {
-            System.out.println(error.getLocalizedMessage());
-        }
-        return result;
-    }
-    public double power_function(double x1,double x2)
-    {
-        double result=0;
-        try
-        {
-            if(x1==0 && x2==0)
-            {
-                result=Double.NaN;
-                throw new ArithmeticException("Pow(0,0) is not possible");
-            }
-            else
-            {
-                result=Math.pow(x1, x2);
-            }
-            return result;
-        }
-        catch (ArithmeticException error)
-        {
-            System.out.println(error.getLocalizedMessage());
-        }
-        return result;
-    }
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
     public static void main(String[] args)
     {
         Calculator calculator = new Calculator();
@@ -125,6 +52,94 @@ public class Calculator
             ch = scanner.next().charAt(0);
         } while (ch == 'y');
 
+    }
+
+    public double sqrt(double x)
+    {
+        double result=0;
+        logger.info("[SQ ROOT] - " + x);
+        try
+        {
+            if(x<0)
+            {
+                result=Double.NaN;
+                throw new ArithmeticException("square root of negative number is not possible");
+            }
+            else
+            {
+                result=Math.sqrt(x);
+                logger.info("[RESULT - SQ ROOT] - " + result);
+            }
+        }
+        catch (ArithmeticException error)
+        {
+            System.out.println(error.getLocalizedMessage());
+        }
+        return result;
+
+    }
+    public double factorial(double x)
+    {
+        logger.info("[FACTORIAL] - " + x);
+        double result=fact(x);
+        logger.info("[RESULT - FACTORIAL] - " + result);
+        return result;
+    }
+    public double fact(double x)
+    {
+        Integer temp=1;
+        for(Integer i=1;i<=x;i++)
+        {
+            temp=i*temp;
+        }
+        return temp;
+    }
+    public  double logarithm(double x)
+    {
+        double result=0;
+        logger.info("[NATURAL LOG] - " + x);
+        try
+        {
+            if(x<=0)
+            {
+                result=Double.NaN;
+                throw new ArithmeticException("Natural Log for 0 and negative number is not possible");
+            }
+            else
+            {
+                result=Math.log(x);
+                logger.info("[RESULT - NATURAL LOG] - " + result);
+            }
+        }
+        catch (ArithmeticException error)
+        {
+            System.out.println(error.getLocalizedMessage());
+        }
+        return result;
+    }
+    public double power_function(double x1,double x2)
+    {
+        double result=0;
+        logger.info("[POWER - " + x1 + " RAISED TO] " + x2);
+        try
+        {
+            if(x1==0 && x2==0)
+            {
+                result=Double.NaN;
+                throw new ArithmeticException("Pow(0,0) is not possible");
+            }
+            else
+            {
+                result=Math.pow(x1, x2);
+                logger.info("[RESULT - POWER] - " + result);
+            }
+            return result;
+        }
+        catch (ArithmeticException error)
+        {
+            System.out.println(error.getLocalizedMessage());
+        }
+        return result;
     }
 
 }
